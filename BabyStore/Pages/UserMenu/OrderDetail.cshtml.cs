c45d9977d5ad8375d5c3a78a4ae09e85a1233063
Tama0026 <mtam7896@gmail.com>
2024-07-23 01:40:39 +0700

    feat/view order detail... wwait update orderid

---
+++ BabyStore/Pages/UserMenu/OrderDetail.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Interfaces;
+using BOs.Entities;
+
+namespace BabyStore.Pages.UserMenu
+{
+    public class OrderDetailModel : PageModel
+    {
+        private readonly IOrderDetailService _orderDetailService;
+        private readonly IOrderService _orderService;
+
+        public OrderDetailModel(IOrderDetailService orderDetailService, IOrderService orderService)
+        {
+            _orderDetailService = orderDetailService;
+            _orderService = orderService;
+        }
+
+        public Order Order { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        public void OnGet(Guid id) // Ensure the parameter type matches the ID type
+        {
+            Order = _orderService.GetOrder(id.ToString()); // Adjust method if necessary
+            if (Order != null)
+            {
+                OrderDetails = _orderDetailService.GetOrderDetails(Order.OrderId);
+            }
+        }
+    }
+}
---
+++ Repos/Interfaces/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using BOs.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repos.Interfaces
+{
+    public interface IOrderDetailRepository
+    {
+        public List<OrderDetail> GetOrderDetails(string orderId);
+    }
+}
