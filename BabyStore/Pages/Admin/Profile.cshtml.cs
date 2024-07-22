3c84fe08edd440ef2b6d033dc9309c3d28aede92
Fancy <dlhminh0506@gmail.com>
2024-07-22 21:45:40 +0700

    Feat: Add Profile page + CSS, Fix direction page in sidebar

--- BabyStore/Pages/Admin/Profile.cshtml.cs
+++ BabyStore/Pages/Admin/Profile.cshtml.cs
@@ -1,12 +1,56 @@
+using BOs.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Interfaces;
 
 namespace BabyStore.Pages.Admin
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
+        private readonly IUserService _userService;
+
+        public ProfileModel(IUserService userService)
         {
+            _userService = userService;
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(String? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _userService.getUserByid(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id, User user)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _userService.Update(id, user);
+
+            return Page();
         }
     }
 }
