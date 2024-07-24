6ca876c2d5154ecfc4184b5cce624a2b34d04182
Tama0026 <mtam7896@gmail.com>
2024-07-24 17:25:58 +0700

    feat/order detail for user

--- BabyStore/Pages/UserMenu/OrderDetail.cshtml.cs
+++ BabyStore/Pages/UserMenu/OrderDetail.cshtml.cs
@@ -1,30 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Services.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BOs;
 using BOs.Entities;
+using Services.Interfaces;
 
 namespace BabyStore.Pages.UserMenu
 {
     public class OrderDetailModel : PageModel
     {
-        private readonly IOrderDetailService _orderDetailService;
         private readonly IOrderService _orderService;
+        private readonly IOrderDetailService _orderDetailService;
 
-        public OrderDetailModel(IOrderDetailService orderDetailService, IOrderService orderService)
+        public OrderDetailModel(IOrderService orderService, IOrderDetailService orderDetailService)
         {
-            _orderDetailService = orderDetailService;
             _orderService = orderService;
+            _orderDetailService = orderDetailService;
         }
 
-        public Order Order { get; set; }
-        public List<OrderDetail> OrderDetails { get; set; }
+        public Order Order { get; set; } = default!;
+        public IList<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
 
-        public void OnGet(string id) // Ensure the parameter type matches the ID type
+        public async Task<IActionResult> OnGetAsync(string id)
         {
-            Order = _orderService.GetOrderById(id); // Adjust method if necessary
-            if (Order != null)
+            if (id == null)
             {
-                OrderDetails = _orderDetailService.GetOrderDetails(Order.OrderId);
+                return NotFound();
             }
+
+            Order = _orderService.GetOrderById(id);
+            if (Order == null)
+            {
+                return NotFound();
+            }
+
+            OrderDetails = _orderDetailService.GetOrderDetails(id);
+            return Page();
         }
     }
 }
