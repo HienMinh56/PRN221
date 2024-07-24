a7eb731ad40a309b1db0c55a0ce8ef486e87a102
NguyenDucLinh411 <linhnguyen41103@gmail.com>
2024-07-25 04:09:47 +0700

    fix validate

--- BOs/Entities/User.cs
+++ BOs/Entities/User.cs
@@ -10,18 +10,19 @@ public partial class User
 
     public string UserId { get; set; } = null!;
 
-    [StringLength(50, MinimumLength = 5, ErrorMessage = "FullName must be between 5 and 50 characters long.")]
-    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "FullName can only contain letters and spaces.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "FullName must be between 2 and 50 characters long.")]
+    [RegularExpression(@"^([A-Z][a-z]*)(\s[A-Z][a-z]*)*$", ErrorMessage = "Each word in FullName must start with an uppercase letter and can only contain letters and spaces.")]
     public string FullName { get; set; } = null!;
 
-    [StringLength(50, MinimumLength = 5, ErrorMessage = "UserName must be between 5 and 50 characters long.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "UserName must be between 5 and 50 characters long.")]
     [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "UserName can only contain letters and numbers with no spaces.")]
     public string UserName { get; set; } = null!;
 
-    [StringLength(50, MinimumLength = 3, ErrorMessage = "Password must be between 3 and 50 characters long.")]
+    [StringLength(200, MinimumLength = 3, ErrorMessage = "Password must be more than 3 characters")]
+    [RegularExpression(@"^\S.*$", ErrorMessage = "Password cannot start with a space.")]
     public string Password { get; set; } = null!;
 
-    [StringLength(200, MinimumLength = 20, ErrorMessage = "Address must be between 10 and 200 characters long.")]
+    [StringLength(200, MinimumLength = 5, ErrorMessage = "Address must be between 5 and 200 characters long.")]
     public string Address { get; set; } = null!;
 
     [EmailAddress(ErrorMessage = "Invalid email address.")]
--- BabyStore/Program.cs
+++ BabyStore/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<GCSConfigOptions>(builder.Configuration);
 
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSession();
