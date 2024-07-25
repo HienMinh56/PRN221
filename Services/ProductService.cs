5150c1a6b90585da0ab18cb442bc8c8e90735a23
TamMinhLinhTinh <93772045+Tama0026@users.noreply.github.com>
2024-07-25 15:52:09 +0700

    Revert "fix: add failed, updated failed, bỏ context trong delete"

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -27,29 +27,11 @@ namespace Services
         public List<Product> GetProducts() => productRepo.GetProducts();
         public async Task AddProduct(Product product)
         {
-            bool productExists = await productRepo.ProductExists(product.Name);
-            if (productExists)
-            {
-                throw new Exception("Product already exists");
-            }
-
             await productRepo.AddProduct(product);
         }
         public Product GetProductById(string productId) => productRepo.GetProductById(productId);
         public async Task UpdateProduct(string productId, Product product)
         {
-            var existingProduct = productRepo.GetProductById(productId);
-            if (existingProduct == null)
-            {
-                throw new Exception("Product not found");
-            }
-
-            bool nameExists = await productRepo.ProductExists(product.Name, productId);
-            if (nameExists && existingProduct.Name != product.Name)
-            {
-                throw new Exception("productName already exists");
-            }
-
             await productRepo.UpdateProduct(productId, product);
         }
         public void DeleteProduct(string productId) => productRepo.DeleteProduct(productId);
