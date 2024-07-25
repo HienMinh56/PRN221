8e8915727122168d0639a348a9bbd50b690d566d
vominhman1611 <minhmanadver@gmail.com>
2024-07-25 17:45:38 +0700

    fix: add failed, create failed, bỏ context trong delete

--- BabyStore/Pages/Admin/ProductManagement/Create.cshtml.cs
+++ BabyStore/Pages/Admin/ProductManagement/Create.cshtml.cs
@@ -38,6 +38,9 @@ namespace BabyStore.Pages.Admin.ProductManagement
 
         [BindProperty]
         public Product Product { get; set; } = default!;
+        [Required(ErrorMessage = "Choose one File!")]
+        [DataType(DataType.Upload)]
+        [FileExtensions(Extensions = "jpg,jpeg,jpe,bmp,gif,png")]
         [BindProperty]
         public IFormFile? Image { get; set; }
 
